a10e545bdfeb598d4bbc2ee37a660111b9bd07c4
lanou <1026366384@qq.com>
2016-05-25 15:50:50 +0800

    新建了一个项目

---
+++ fjxProject/fjxProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fjxProject
+//
+//  Created by lanou on 16/5/25.
+//  Copyright © 2016年 FJX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
