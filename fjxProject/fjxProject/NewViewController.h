9cd527066197cc6528547c5d2e012b0b3e7200b5
lanou <1026366384@qq.com>
2016-05-25 16:05:31 +0800

    no message

---
+++ fjxProject/fjxProject/NewViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewViewController.h
+//  fjxProject
+//
+//  Created by lanou on 16/5/25.
+//  Copyright © 2016年 FJX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewViewController : UIViewController
+
+@end
--- fjxProject/fjxProject/ViewController.m
+++ fjxProject/fjxProject/ViewController.m
@@ -17,6 +17,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    int number = 0;
+    int sum = number++;
+    
+    NSLog(@"sum__%d", sum);
 }
 
 - (void)didReceiveMemoryWarning {
